80f77416879fafdd988a4fada4daf4727965731e
Данилов Дмитрий <danilov.da.ufa@gmail.com>
2022-06-15 20:30:53 +0500

    second commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://klike.net/uploads/posts/2020-04/1587719791_1.jpg"
+        alt="LOGO"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://www.komar.de/ru/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/S/H/SHX8-087_1568286487.jpg"
+          alt="toplogo"
+        />
+      </div>
+      <div>ava+discription</div>
+      <div>
+        my post
+        <div>new post</div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
