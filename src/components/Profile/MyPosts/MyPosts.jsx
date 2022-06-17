063849efdbf97490be572da3c4c9727f8607fcd1
Данилов Дмитрий <danilov.da.ufa@gmail.com>
2022-06-17 15:48:02 +0500

    lesson 16

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,11 +10,12 @@ const MyPosts = () => {
         <button>Add post</button>
       </div>
       <div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post message="Hi, first message" likecounts="15" />
+        <Post message="second post" likecounts="22" />
+        <Post message="3" likecounts="33" />
+        <Post message="4" />
+        <Post message="five" />
+        <Post message="SIX" />
       </div>
     </div>
   );
