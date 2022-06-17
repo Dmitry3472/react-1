f09b13067374df0c30f102c10f1e9c746f60b5c5
Данилов Дмитрий <danilov.da.ufa@gmail.com>
2022-06-17 13:49:12 +0500

    lesson 15

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={s.header}>
       <img
         src="https://klike.net/uploads/posts/2020-04/1587719791_1.jpg"
         alt="LOGO"
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import s from "./Profile.module.css";
+import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img
           src="https://www.komar.de/ru/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/S/H/SHX8-087_1568286487.jpg"
@@ -10,12 +12,7 @@ const Profile = () => {
         />
       </div>
       <div>ava+discription</div>
-      <div>
-        my post
-        <div>new post</div>
-        <div>post 1</div>
-        <div>post 2</div>
-      </div>
+      <MyPosts />
     </div>
   );
 };
