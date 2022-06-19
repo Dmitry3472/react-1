79752bf20117be36ed9a2a1a30499f1ef3985c71
Данилов Дмитрий <danilov.da.ufa@gmail.com>
2022-06-20 00:32:40 +0500

    24 lesson

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -22,25 +22,40 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
+  let dialogsData = [
+    { id: 1, name: "User1" },
+    { id: 2, name: "User2" },
+    { id: 3, name: "User3" },
+    { id: 4, name: "User4" },
+    { id: 5, name: "User5" },
+    { id: 6, name: "User6" },
+  ];
+  let messagesData = [
+    { id: 1, message: "Message1" },
+    { id: 2, message: "Message2" },
+    { id: 3, message: "Message3" },
+    { id: 4, message: "Message4" },
+    { id: 5, message: "Message5" },
+    { id: 6, message: "Message6" },
+  ];
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <DialogsItem name="User1" id="1" />
-        <DialogsItem name="User2" id="2" />
-        <DialogsItem name="User3" id="3" />
-        <DialogsItem name="User4" id="4" />
-        <DialogsItem name="User5" id="5" />
+        <DialogsItem name={dialogsData[0].name} id={dialogsData[0].id} />
+        <DialogsItem name={dialogsData[1].name} id={dialogsData[1].id} />
+        <DialogsItem name={dialogsData[2].name} id={dialogsData[2].id} />
+        <DialogsItem name={dialogsData[3].name} id={dialogsData[3].id} />
+        <DialogsItem name={dialogsData[4].name} id={dialogsData[4].id} />
       </div>
       <div className={s.messages}>
-        <Message message="Message 1" />
-        <Message message="Message 2" />
-        <Message message="Message 3" />
-        <Message message="Message 4" />
+        <Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />
+        <Message message={messagesData[2].message} />
+        <Message message={messagesData[3].message} />
       </div>
     </div>
   );
 };
 
 export default Dialogs;
-
-//className={(navData) => (navData.isActive ? s.active : s.dialog)}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,15 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post.jsx";
 
 const MyPosts = () => {
+  let postData = [
+    { id: 1, message: "MyMessage1", likeCounts: "15" },
+    { id: 2, message: "MyMessage2", likeCounts: "22" },
+    { id: 3, message: "MyMessage3", likeCounts: "33" },
+    { id: 4, message: "MyMessage4" },
+    { id: 5, message: "MyMessage5" },
+    { id: 6, message: "MyMessage6" },
+  ];
+
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
@@ -15,12 +24,21 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={s.posts}>
-        <Post message="Hi, first message" likeCounts="15" />
-        <Post message="second post" likecCounts="22" />
-        <Post message="3" likeCounts="33" />
-        <Post message="4" />
-        <Post message="five" />
-        <Post message="SIX" />
+        <Post
+          message={postData[0].message}
+          likeCounts={postData[0].likeCounts}
+        />
+        <Post
+          message={postData[1].message}
+          likeCounts={postData[1].likeCounts}
+        />
+        <Post
+          message={postData[2].message}
+          likeCounts={postData[2].likeCounts}
+        />
+        <Post message={postData[3].message} />
+        <Post message={postData[4].message} />
+        <Post message={postData[5].message} />
       </div>
     </div>
   );
