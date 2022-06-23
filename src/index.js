89f1555fc5c8c9f4cbf27b16e036f09a56193b1e
Данилов Дмитрий <danilov.da.ufa@gmail.com>
2022-06-24 01:54:46 +0500

    35 lesson

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import state from './redux/state';
-import { rerenderEntireTree } from './render';
+import state, { subscribe } from './redux/state';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { addPost, updateNewPostText } from './redux/state';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+let rerenderEntireTree = (state) => {
+
+  root.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+    </React.StrictMode>
+  );
+}
+
+subscribe(rerenderEntireTree);
 
 rerenderEntireTree(state);
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,9 @@
 
-import { rerenderEntireTree } from './../render';
+let rerenderEntireTree = () => {
+    console.log('State changed')
+
+}
+
 
 let state = {
     profilePage: {
@@ -36,7 +40,7 @@ let state = {
     }
 }
 
-export let addPost = () => {
+export const addPost = () => {
 
     let newPost = {
         id: 5,
@@ -48,11 +52,14 @@ export let addPost = () => {
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
 
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
 }
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 
+}
 
 export default state;
