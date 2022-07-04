102244126251e4dd7567e0a743d69d7570813205
Данилов Дмитрий <danilov.da.ufa@gmail.com>
2022-07-05 00:12:06 +0500

    43 lesson

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = (state) => {
-  debugger
+
   root.render(
     <React.StrictMode>
       <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
