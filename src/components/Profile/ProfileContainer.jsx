50620addd9313b6ca8ad1f2e7505b1ccb5f755e3
Данилов Дмитрий <danilov.da.ufa@gmail.com>
2022-08-12 01:10:21 +0500

    60 lesson (added actual user id to url)

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,24 @@ import { connect } from "react-redux";
 import Profile from "./Profile";
 import * as axios from "axios";
 import { setUserProfile } from "../../redux/profile-reducer";
+import { useParams } from "react-router-dom";
+
+export function withRouter(Children) {
+  return (props) => {
+    const match = { params: useParams() };
+    return <Children {...props} match={match} />;
+  };
+}
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
+    debugger;
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
     axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
         debugger;
         this.props.setUserProfile(response.data);
@@ -23,4 +36,8 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, { setUserProfile })(
+  WithUrlDataContainerComponent
+);
